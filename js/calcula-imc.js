820ed1768b8f2045f4bb61839872426ac61872b0
Giovanna Alyssa <giovanna.alyssa@rdstation.com>
2023-02-22 11:16:45 -0300

    add validation and error message

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,26 +1,28 @@
 var pacientes = document.querySelectorAll(".paciente");
 
 for(var i = 0; i < pacientes.length ; i++){
+
     var paciente = pacientes[i];
+
     var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
     var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
     var tdImc = paciente.querySelector(".info-imc");
 
-    var peso = tdPeso.textContent;
-    var altura = tdAltura.textContent;
+    var alturaEhValida = validaAltura(altura);
+    var pesoEhValido = validaPeso(peso);
 
-    var alturaEhValida = true;
-    var pesoEhValido = true;
-
-    if(peso <= 0 || peso >= 1000){
+    if(!pesoEhValido){
         console.log("Peso inválido");
         tdImc.textContent = "Peso inválido!";
         pesoEhValido = false;
         paciente.classList.add("paciente-invalido");
     }
 
-    if(altura <= 0 || altura >= 3.0){
+    if(!alturaEhValida){
         console.log("Altura inválida");
         tdImc.textContent = "Altura inválida!";
         alturaEhValida = false;
@@ -35,8 +37,25 @@ for(var i = 0; i < pacientes.length ; i++){
 
     function calculaImc(peso,altura){
         var imc = 0;
-
         var imc = peso / ( altura * altura);
 
         return imc.toFixed(2);;
+    }
+
+    function validaPeso(peso){
+
+        if (peso >= 0 && peso <= 1000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function validaAltura(altura) {
+
+        if (altura >= 0 && altura <= 3.0) {
+            return true;
+        } else {
+            return false;
+        }
     }
